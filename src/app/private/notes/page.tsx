8d15220a715b4d82efb736a56719efd10010a788
rui775 <rsilva6420.dev@gmail.com>
2024-02-16 14:57:16 +0000

    fix: supabase configuration; add: notes page to test fetch

---
+++ src/app/private/notes/page.tsx
@@ -0,0 +1,18 @@
+
+import { createClient } from '../../../../supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const { data: todos } = await supabase.from('todos').select()
+
+    return (
+        <ul>
+            {todos?.map((todo, index) => (
+                <li key={index}>{todo}</li>
+            ))}
+        </ul>
+    )
+}
