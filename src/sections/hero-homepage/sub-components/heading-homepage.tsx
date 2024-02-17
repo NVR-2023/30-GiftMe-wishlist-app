e81411991bc753ac882968de2aa7646b6cb76cbc
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-17 09:43:40 +0000

    chore: change homepage stykes

--- src/sections/hero-homepage/sub-components/heading-homepage.tsx
+++ src/sections/hero-homepage/sub-components/heading-homepage.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const HeadingHomepage: FC = () => {
   return (
-    <div className="tracking-[-0.02em] text-2xl font-black bg-gradient-to-r to-violet-400 from-fuchsia-800 bg-clip-text text-transparent">
+    <div className="tracking-[-0.02em] text-2xl font-bold bg-gradient-to-r to-violet-400 from-fuchsia-800 bg-clip-text text-transparent">
       GiftMe:
     </div>
   );
--- src/sections/hero-homepage/sub-components/tagline-homepage.tsx
+++ src/sections/hero-homepage/sub-components/tagline-homepage.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const TaglineHomepage: FC = () => {
   return (
-    <div className="tracking-[-0.02em] text-4xl font-black bg-gradient-to-r from-fuchsia-800 to-violet-400  bg-clip-text text-transparent">
+    <div className="tracking-[-0.02em] text-4xl font-bold bg-gradient-to-r from-fuchsia-800 to-violet-400  bg-clip-text text-transparent">
       Hassle-free wish lists{" "}
     </div>
   );
