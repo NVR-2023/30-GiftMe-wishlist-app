bd8a8a92d472baecf3b9b7a653c4d6573d02aed4
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-17 00:02:31 +0000

    chore: tune homepage  typography

--- src/sections/hero-homepage/sub-components/summary-homepage.tsx
+++ src/sections/hero-homepage/sub-components/summary-homepage.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const SummaryHomepage: FC = () => {
   return (
-    <summary className="space-y-[.3rem] font-sm font-semibold bg-gradient-to-r to-violet-400 from-fuchsia-800 bg-clip-text text-transparent tracking-wide ">
+    <summary className="space-y-[.05rem]  font-semibold bg-gradient-to-r to-violet-400 from-fuchsia-800 bg-clip-text text-transparent tracking-wide ">
       <p>Easy-peasy large lists</p>
       <p>Password-protected groups </p>
       <p>Auto-fetched & auto-tracked prices</p>
