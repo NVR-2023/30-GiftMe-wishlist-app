97c043eaca5e96aee852e6f88dc7b13e5b50e4fb
NVR <nunoviolanterodrigues@hotmail.com>
2024-02-25 14:51:55 +0000

    feat: create groups page with input

--- src/app/private/groups/page.tsx
+++ src/app/private/groups/page.tsx
@@ -1,7 +1,10 @@
 const Groups = () => {
   return (
-    <div>
-      <input type="text">Create group</input>
+    <div className="h-screen w-screen p-10">
+      <div className="flex flex-col">
+        <span className="">Create group</span>
+        <input type="text" className=" w-36 border-black rounded border-2" />
+      </div>
     </div>
   );
 };
