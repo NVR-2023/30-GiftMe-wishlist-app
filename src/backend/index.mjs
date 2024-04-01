d31d1414b82d7b381f90f11c9014cc6d5e8ea3f2
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2024-04-01 12:18:29 +0200

    fix: file extension server

---
+++ src/backend/index.mjs
@@ -0,0 +1,13 @@
+// THIS FILE IS TO INITIALIZE THE SERVER WITH EXPRESS
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
